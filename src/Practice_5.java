3ec459f6fd8bca02929ee1f32e185f7a466fdb35
Alekzander <alekzanderor@gmail.com>
2020-03-28 14:57:37 +0400

    Initial commit

---
+++ src/Practice_5.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Practice_5 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number: ");
+        int number = sc.nextInt();
+
+        int a=number;
+        System.out.println(a);
+        a=a+number;
+        System.out.println(a);
+        a=a+number;
+        System.out.println(a);
+        a=a+number;
+        System.out.println(a);
+        a=a+number;
+        System.out.println(a);
+        a=a+number;
+        System.out.println(a);
+        a=a+number;
+        System.out.println(a);
+        a=a+number;
+        System.out.println(a);
+        a=a+number;
+        System.out.println(a);
+        a=a+number;
+        System.out.println(a);
+    }
+}
